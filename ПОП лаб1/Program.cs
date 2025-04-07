fb0526aa674b8f414eccd20578fb578cb166c09e
Bohdan-Golubiev <holubiev.bohdan1122@vu.cdu.edu.ua>
2025-04-07 13:51:01 +0300

    Добавьте файлы проекта.

---
+++ ПОП лаб1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ПОП_лаб1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int threadNumber = i+1;
+                Thread thread = new Thread(() => CalculateSum(threadNumber));
+                thread.Start();
+            }
+
+            Thread controller = new Thread(TimeController);
+            controller.Start();
+        }
+        static void CalculateSum(int threadNumber)
+        {
+            long sum = 0;
+            while (!canStop)
+            {
+                sum++;
+            }
+            Console.WriteLine("Thread " + threadNumber + ": sum - " + sum);
+        }
+
+        static bool canStop = false;
+
+        static void TimeController()
+        {
+            Thread.Sleep(50 * 1000);
+            canStop = true;
+        }
+    }
+}
